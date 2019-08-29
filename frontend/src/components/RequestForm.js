bdbf354970a95c6fb2fe09f494c660a2ac1edfbe
jennl97 <jennhott97@gmail.com>
2019-08-28 22:29:57 -0500

    5th commit of Frontend

--- frontend/src/components/RequestForm.js
+++ frontend/src/components/RequestForm.js
@@ -1,25 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Form, Field, withFormik } from "formik";
 import axios from "axios";
 import * as Yup from "yup";
 
-function RequestForm(errors, touched, status){
+class RequestForm extends React.Component{
+    state = {
+        name: "",
+        // parks: "",
+        // ride: "",
+        // request: "",
+        // number: "", 
+        // age: "",
+        comments: "",
+        id: "",
+        time: ""
+    }
+    
+     handleChange = event => {
+         this.setState({[event.target.name]: event.target.value});
+         console.log(this.state);
+        };
+
+        handleSubmit = event => {
+            event.preventDefault();
+            axios
+            .post('https://disneyparent-backend.herokuapp.com/posts', this.state)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+
+    render(){
     return (
         <div className="requestForm">
             
-                <Form>
+                <Form onSubmit = {this.handleSubmit}>
                     <Field
                     component="input"
                     type="text"
                     name="name"
                     placeholder="Name"
+                    value = {this.state.name}
+                    onChange={this.handleChange}
                     />
-                    {touched.name && errors.name && (
+                    {/* {touched.name && errors.name && (
                         <p className="error">{errors.name}</p>
-                    )}
+                    )} */}
+                     <Field
+                    component="input"
+                    type="text"
+                    name="id"
+                    placeholder="ID Number"
+                    value = {this.state.id}
+                    onChange={this.handleChange}
+                    />
+                     <Field
+                    component="input"
+                    type="text"
+                    name="time"
+                    placeholder="Time for request"
+                    value = {this.state.time}
+                    onChange={this.handleChange}
+                    />
     
                     <Field
-                    component="select" className="park-select" name="parks">
+                    component="select" className="park-select" name="parks"  value = {this.state.parks}
+                    onChange={this.handleChange}>
                     <option>Select A Park</option>
                     <option value="magicKingdom">Magic Kingdom</option>
                     <option value="animalKingdom">Animal Kingdom</option>
@@ -32,9 +81,12 @@ function RequestForm(errors, touched, status){
                     type="text"
                     name="ride"
                     placeholder="Nearest Ride"
+                    value = {this.state.ride}
+                    onChange={this.handleChange}
                     />
                     <Field
-                    component="select" className="park-select" name="request">
+                    component="select" className="park-select" name="request"  value = {this.state.request}
+                    onChange={this.handleChange}>
                         <option>Type of Request</option>
                         <option value="strollerExchange">Stroller Exchange</option>
                         <option value="parentSwap">Parent Swap</option>
@@ -44,18 +96,24 @@ function RequestForm(errors, touched, status){
                     type="number"
                     name="number"
                     placeholder="Number of Children"
+                    value = {this.state.number}
+                    onChange={this.handleChange}
                     />
                    <Field
                     component="input"
                     type="text"
                     name="age"
                     placeholder="Age of Children"
+                    value = {this.state.age}
+                    onChange={this.handleChange}
                     />
                     <Field
                     component="textarea"
                     type="text"
                     name="comments"
                     placeholder="Comments"
+                    value = {this.state.coments}
+                    onChange={this.handleChange}
                     />
                     <button type="submit">Submit</button>
                 </Form>
@@ -64,7 +122,16 @@ function RequestForm(errors, touched, status){
 
         </div>
     )
+ }
 }
+
+
+
+
+
+
+
+
 
 const propsToValuesMap = withFormik({
     mapPropsToValues({name, parks, ride, request, number, age, comments}){
@@ -83,7 +150,9 @@ const propsToValuesMap = withFormik({
        name: Yup.string().required("Parent's name is required")
     //    parks: Yup.array().required("Park is required")
        
-    })   
+    }),
+    
+
 });
 
 const RequestFormFormik = propsToValuesMap(RequestForm);
