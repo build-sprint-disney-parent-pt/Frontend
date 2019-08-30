547bb8c903db42e16db8f902cd49b7cd870bed3d
Megan Jeffcoat <44781690+meganjeffcoat@users.noreply.github.com>
2019-08-29 22:42:38 -0500

    Revert "routing updating"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,15 @@ import './App.css';
 
 //import components
 import ParentLogin from "./components/ParentLogin";
-import RequestForm from "./components/RequestForm";
-import MessageBoardList from "./components/MessageBoardList";
-import MessageBoard from './components/MessageBoard';
-
+import Links from "./components/Links";
 
 function App() {
-  // const [ RequestForm, setRequestForm] = React.useState("");
-
   return (
     <div className="App">
       <header>
         <nav>
           <div className='nav-links'>
-            <Link className="links" to="/parent">Home</Link>
-            {/* <NavLink exact className="some-link" to="/">Request form</NavLink> */}
-            <Link className="links" to="/request">Request Form</Link>
-            <Link className="links" to="/message">Message Board</Link>
+           <Links />
           </div>
         </nav>
       </header>
@@ -29,17 +21,10 @@ function App() {
       <h1>Parent Login</h1>
       </div>
       <div className="form">
-     <Route path="/parent" component={ParentLogin}/>
+     <ParentLogin />
+     
      </div>
-     <div className="form">
-       <h1>Request Form</h1>
-     <Route path="/request" component={RequestForm}/>
-     </div>
-     <div className="messageBoard">
-       <h1>Message Board</h1>
-     <Route path="/message" component={MessageBoard}/>
-     </div>
-      </div>
+    </div>
   );
 }
 
